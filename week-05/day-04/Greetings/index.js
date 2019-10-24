aaea862b307b74eef53d4fb752af54c19be401f6
Garrin Wang <Garrin_Wang@epam.com>
2019-10-24 15:28:47 +0800

    done greeting

---
+++ week-05/day-04/Greetings/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+	res.render('home', {
+		name: req.query.name
+	})
+})
+// start express app on port 3000
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
